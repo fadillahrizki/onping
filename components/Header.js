d37862cac598980a1da9eb3d849e3dd96b8979a5
RaioneruD <dasutinraioneru@gmail.com>
2022-06-25 22:13:33 +0700

    update header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaShoppingCart, FaBell, FaHistory, FaEnvelope, FaUserCircle, FaArrowAltCircleDown } from 'react-icons/fa';
-
+import Link from 'next/link';
 
 export default function Header(){
     return(
@@ -12,16 +12,16 @@ export default function Header(){
                     <li>Buka OnShop</li>
                     <li>Bantuan</li>
                 </ul>
-                <p className='text-[13px] mr-5 flex gap-[7px]'><span className='mt-1'>{<FaArrowAltCircleDown/>}</span>Download Aplikasi</p>
+                <p className='text-[13px] mr-5 flex gap-[7px]'><span>{<FaArrowAltCircleDown size={20}/>}</span>Download Aplikasi</p>
             </nav>
             <div className='ml-24 mt-3 flex gap-5'>
                 <h1 className='text-[50px]'>
-                    <Link to=''></Link>
+                    <Link href='/'>ONP!NG</Link>
                 </h1>
                 <form className='mt-4'>   
                     <input type="search" id="default-search" className="p-3 bg-zinc-200 w-[500px] rounded-lg" placeholder="Cari..." required />
                 </form>
-                <div className='flex mt-6 gap-[50px] ml-10'>
+                <div className='flex mt-6 gap-[70px] ml-20'>
                     {<FaShoppingCart size={30} />}
                     {<FaBell size={30} />}
                     {<FaHistory size={30} />}
