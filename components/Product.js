ac7e65409710959556c2509dd540580b359a3109
RaioneruD <dasutinraioneru@gmail.com>
2022-06-25 14:55:16 +0700

    update data produk .json

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,19 @@
-export default function Product(){
-    return (
-        <div>
-            Product
-        </div>
+import Image from 'next/image';
+import { Fragment } from 'react';
+import Poco from '../public/UAS-Assets/Elektronik/Poco.jpg'
+import Smart from '../public/UAS-Assets/Elektronik/Smart.png'
+import MITV from '../public/UAS-Assets/Elektronik/MITV.png'
+import Samsung from '../public/UAS-Assets/Elektronik/samsung.png'
+import Redmi from '../public/UAS-Assets/Elektronik/Redmi.png'
+
+export default function Product(props){
+    return(
+        <Fragment>
+            <Image src={MITV} alt="11t" width="386" height="343" />
+            <Image src={Redmi} alt="11t" width="386" height="343" />
+            <Image src={Poco} alt="11t" width="386" height="343" />
+            <Image src={Smart} alt="11t" width="386" height="343" />
+            <Image src={Samsung} alt="11t" width="386" height="343" />
+        </Fragment>
     )
 }
